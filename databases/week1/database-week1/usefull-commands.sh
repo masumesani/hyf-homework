fac894eedee0a9e2ac28d29e7f6b3b534ba7e9c4
masumesani <masume.sani@gmail.com>
2020-01-29 00:43:41 +0100

    homework for database_week1

---
+++ databases/week1/database-week1/usefull-commands.sh
@@ -0,0 +1,8 @@
+sudo mysql -u root -p
+
+-- when try create a database using your vs code.
+ 1) create database hyf_lesson1;
+
+
+ 2) source /home/masi/projects/hyf/hyf-database-homework/database-week1/classtimedataw1.sql;
+
