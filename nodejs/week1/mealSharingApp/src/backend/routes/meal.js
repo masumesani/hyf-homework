a2a2e8b2f6a7b69c43b3c7c38353cec925b0955c
masume sani <masume.sani@gmail.com>
2020-03-26 15:50:06 +0100

    pr reviews fixes

--- nodejs/week1/mealSharingApp/src/backend/routes/meal.js
+++ nodejs/week1/mealSharingApp/src/backend/routes/meal.js
@@ -1,14 +1,13 @@
-const dataHelper = require('../data/dataHelper')
+const dataHelper = require("../data/dataHelper");
 
 module.exports = (req, res) => {
+  const meals = dataHelper.getMeals();
 
-    const meals = dataHelper.getMeals();
+  const randomMealIndex = Math.floor(Math.random() * meals.length);
 
-    const randomMealIndex =  Math.floor(Math.random()* meals.length);
-    
-    const meal = meals[randomMealIndex];
-    
-    const result = dataHelper.getMealsAugmentedWithReviews([meal])
+  const meal = meals[randomMealIndex];
 
-    res.send(result);
-}
+  const result = dataHelper.getMealsAugmentedWithReviews([meal]);
+
+  res.send(result[0]);
+};
