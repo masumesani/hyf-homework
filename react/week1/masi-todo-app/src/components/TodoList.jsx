bb527287bc418b60fac1bd6262b991d0e15b8e4d
masume sani <masume.sani@gmail.com>
2020-03-27 13:44:23 +0100

    -renaming ListItem.jsx and TodoList.jsx - fixed todo list based on PR

--- react/week1/masi-todo-app/src/components/TodoList.jsx
+++ react/week1/masi-todo-app/src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ListItem from "./listItem";
+import ListItem from "./ListItem";
 
 const initialMockedTodoes = [
   { id: 1, title: "Get out of bed", date: new Date("09-13-2017"), done: true },
@@ -10,14 +10,12 @@ const initialMockedTodoes = [
 const MyTodoList = () => {
   const [todoList, setTodoList] = useState(initialMockedTodoes);
   const changeState = id => {
-    let index = todoList.findIndex(todo => todo.id === id);
-    let arrayAheadOfTarget = todoList.slice(0, index);
-    let target = Object.assign({}, todoList[index], {
-      done: !todoList[index].done
-    });
-    let arrayAfterTarget = todoList.slice(index + 1);
-    let out = [...arrayAheadOfTarget, target, ...arrayAfterTarget];
-    setTodoList(out);
+    setTodoList(
+      todoList.map(todo => {
+        if (todo.id === id) return { ...todo, done: !todo.done };
+        return todo;
+      })
+    );
   };
   return (
     <div>
